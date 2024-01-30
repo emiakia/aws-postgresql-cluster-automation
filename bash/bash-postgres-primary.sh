a800b8309a01ef028cd7809dab6e96010aadad11
Emran Kia <emran.kia@gmail.com>
2024-01-30 10:25:20 +0100

    initial

--- bash/bash-postgres-primary.sh
+++ bash/bash-postgres-primary.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-local_ip=$(hostname -I | awk '{print $1}')
-echo $local_ip
+# local_ip=$(hostname -I | awk '{print $1}')
+# echo $local_ip
+
+ssh_user=whoami
+echo $ssh_user
+
 
 # Get playbook from Github
 wget https://raw.githubusercontent.com/emiakia/aws_components_ec2_postgres_install_and_ha/main/ansible/p00install-postgres-for-primary.yaml
@@ -13,7 +17,8 @@ wget https://raw.githubusercontent.com/emiakia/aws_components_ec2_postgres_insta
 wget https://raw.githubusercontent.com/emiakia/aws_components_ec2_postgres_install_and_ha/main/ansible/vars.yaml
 wget https://raw.githubusercontent.com/emiakia/aws_components_ec2_postgres_install_and_ha/main/ansible/inventory.yaml
 
-sed -i "s/PPP/$local_ip/" vars.yaml
+sed -i "s/PPP/localhost/" vars.yaml
+sed -i "s/UUU/$ssh_user/" vars.yaml
 
 # Define the path to your Ansible playbook
 PLAYBOOK_PATH="./p00install-postgres-for-primary.yaml"
