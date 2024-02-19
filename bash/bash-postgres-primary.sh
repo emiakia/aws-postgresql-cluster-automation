bb2870edf34feed687fa787cf1206ead81c1b2c0
Emran Kia <emran.kia@gmail.com>
2024-02-19 10:14:10 +0100

    initial

--- bash/bash-postgres-primary.sh
+++ bash/bash-postgres-primary.sh
@@ -16,25 +16,24 @@ wget https://raw.githubusercontent.com/emiakia/aws_components_ec2_postgres_insta
 sed -i "s/PPP/$local_ip/" vars.yaml
 
 # Define the path to your Ansible playbook
-PLAYBOOK_PATH="./p00install-postgres-for-primary.yaml
+PLAYBOOK_PATH="./p00install-postgres-for-primary.yaml"
 echo $PLAYBOOK_PATH
 
-# # Update the system
-# echo "Updating system..."
-# sudo apt update -y
+# Update the system
+echo "Updating system..."
+sudo apt update -y
 
-# # Install Ansible
-# echo "Installing Ansible..."
-# sudo apt install -y ansible
+# Install Ansible
+echo "Installing Ansible..."
+sudo apt install -y ansible
 
-# # Verify Ansible installation
-# echo "Verifying Ansible installation..."
-# ansible --version
+# Verify Ansible installation
+echo "Verifying Ansible installation..."
+ansible --version
 
-# # Run the Ansible playbook
-# echo "Running Ansible playbook..."
-# ansible-playbook -i inventory.yaml "$PLAYBOOK_PATH"
-
+# Run the Ansible playbook
+echo "Running Ansible playbook..."
+ansible-playbook -i inventory.yaml "$PLAYBOOK_PATH"
 
 rm -rf *yaml*
 rm -rf *cfg*
